a0ca11b33bebd14a3f031eec0f2294dcea52bdb7
Egor Rya <fayncore@gmail.com>
2018-05-04 02:23:30 +0700

    Initial commit

---
+++ assets/js/common.js
@@ -0,0 +1,18 @@
+var calcuation = function() {
+    "use strict";
+    var btn = document.getElementById("btn"),
+        h = document.getElementById("height"),
+        w = document.getElementById("weight"),
+        ans = document.getElementById("calculation"),
+        output = "";
+
+    btn.addEventListener("click", function(e) {
+        e.preventDefault();
+        output = ((Math.round(w.value) / ((Math.round(h.value) / 100) * (Math.round(h.value) / 100)))).toFixed(2);
+        if (output > 0) {
+            ans.innerHTML = output + "%";
+        } else {
+            ans.innerHTML = "Invalid dimensions";
+        }
+    });
+}();
