e4f7f99f20ad545c4726ac4ea5ff2626c8308315
Egor Rya <fayncore@gmail.com>
2018-05-04 21:16:16 +0700

    ready

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,34 +4,42 @@ var calcuation = function() {
     h = document.getElementById("height"),
     w = document.getElementById("weight"),
     ans = document.getElementById("calculation"),
+    me = document.getElementById("me"),
     output = "";
 
     btn.addEventListener("click", function(e) {
         e.preventDefault();
-        output = ((Math.round(w.value) / ((Math.round(h.value) / 100) * (Math.round(h.value) / 100)))).toFixed(2);
+        output = (Math.round(w.value) / ((Math.round(h.value) / 100) * (Math.round(h.value) / 100))).toFixed(2);
         if (output > 0) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output + ". У вас выраженный дефицит массы, нужно набирать!";
-        } else if (output > 16) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output;
-            ans.innerHTML ="У вас недостаточная масса тела, стоит поднабрать.";
-        } else if (output > 18.7) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output;
-            ans.innerHTML ="У вас отличный вес!";
-        } else if (output > 25) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output;
-            ans.innerHTML ="У вас есть немного избыточного веса.";
-        } else if (output > 30) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output;
-            ans.innerHTML ="У вас первая степень ожирения. Нужно что-то предпринять.";
-        } else if (output > 35) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output;
-            ans.innerHTML ="У вас вторая степень ожирения. Нужно срочно что-то предпринять!";
-        } else if (output > 40) {
-            ans.innerHTML ="Ваш ИМТ составляет " + output;
-            ans.innerHTML ="У вас третья степень ожирения. Пожалуйста, обратитесь к доктору!";
+            if (output > 16) {
+               if (output > 18.7) {
+                  if (output > 25) {
+                    if (output > 30) {
+                        if (output > 35) {
+                            if (output > 40) {
+                                ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас третья степень ожирения. Пожалуйста, обратитесь к доктору!";
+                            }   else {
+                                ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас вторая степень ожирения. Нужно срочно что-то предпринять!";
+                            }
+                        }   else {
+                            ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас первая степень ожирения. Нужно что-то предпринять.";
+                        }
+                    }    else {
+                        ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас есть немного избыточного веса.";
+                    }
+                }   else {
+                    ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас отличный вес!";
+                } 
+            } else {
+                ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас недостаточная масса тела, стоит поднабрать.";
+            } 
         } else {
-            ans.innerHTML = "У вас отрицательная масса тела, обратитесь к физикам! Вы новое научное открытие!";
+            ans.innerHTML = "Индекс массы вашего тела составляет " + output + ". У вас выраженный дефицит массы, нужно набирать!";
         }
-  });
+    }  else {
+        ans.innerHTML = "У вас отрицательная масса тела, обратитесь к физикам! Вы новое научное открытие! <br/><br/>Ну или ничего не ввели :(";
+    }    me.innerHTML = "Лайкни авку за старания ;)";
+
+});
 }();
 
